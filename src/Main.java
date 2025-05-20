a01c607aa0a5e02eddf74ccc90b8565633c44875
Ladislav-Sandalov <v.sandalov2@yandex.ru>
2025-05-20 16:58:25 +0300

    vrode rabotait

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String code = "x := 42;\ny := x + 1;";
+        String code = "begin x ass 2; y ass x + 1; end.";
         Lexer lexer = new Lexer(code);
         lexer.tokenizeAll();
         lexer.printTables();
+        Parser parser = new Parser(lexer.getLexemeTable().getLexemes());
+        parser.parseProgram();
     }
 }
